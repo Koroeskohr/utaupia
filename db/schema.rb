26a95d6c825083225d25d31e1dbf56aa46999b33
Victor Viale <somebodywas@gmail.com>
2015-11-13 15:36:08 +0100

    Add utauloid migration file

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20151113140838) do
+
+  create_table "utauloids", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.integer  "gender",     null: false
+    t.integer  "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
