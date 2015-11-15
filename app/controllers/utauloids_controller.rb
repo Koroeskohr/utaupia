d90638f3a48e169a8e46f0a273bb3244e5b0e263
Victor Viale <somebodywas@gmail.com>
2015-11-15 14:55:06 +0100

    Add create action for UtauloidsController

--- app/controllers/utauloids_controller.rb
+++ app/controllers/utauloids_controller.rb
@@ -7,4 +7,15 @@ class UtauloidsController < ApplicationController
 		@utauloid = Utauloid.new
 	end
 
+	def create
+		@utauloid = Utauloid.new(utauloid_params)
+		if @utauloid.save
+			redirect_to @utauloid
+		end
+	end
+
+	private
+		def utauloid_params
+			params.require(:utauloid).permit(:age, :gender, :name)
+		end
 end
