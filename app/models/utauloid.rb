59f0c4b11d2006d8f7024a7b7a34c93eae849fe3
Victor Viale <somebodywas@gmail.com>
2015-11-15 16:47:01 +0100

    Add age validation to user

--- app/models/utauloid.rb
+++ app/models/utauloid.rb
@@ -1,6 +1,8 @@
 class Utauloid < ActiveRecord::Base
 	validates :name, presence: true
 	validates :gender, presence: true
+	validates :age, numericality: { only_integer: true, greater_than: 1 }
+
 	
 	enum gender: [:male, :female]
 
