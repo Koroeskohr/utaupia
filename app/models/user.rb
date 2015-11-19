23bd9721187317d650b8aa415d0932ada5cf1e6d
Victor Viale <somebodywas@gmail.com>
2015-11-19 23:12:09 +0100

    Add enum role and set default role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  after_initialize :set_default_role, :if => :new_record?
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -8,6 +10,11 @@ class User < ActiveRecord::Base
 						uniqueness: true, 
 						format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 
+  enum role: [:user, :moderator, :administrator]
+
 	has_many :utauloids
 
+  def set_default_role
+    self.role ||= :user
+  end
 end
