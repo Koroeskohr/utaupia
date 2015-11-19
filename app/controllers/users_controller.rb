38181eafe1e18c071cbeaa17c0cc1f414c1a7db4
Victor Viale <somebodywas@gmail.com>
2015-11-19 22:21:52 +0100

    Remove useless statement

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show, :create, :new]
+  before_action :authenticate_user!, except: [:index, :show, :new]
 
   def index
     @users = User.all
