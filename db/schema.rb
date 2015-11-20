3552b7685e222a5b95ba071916a19a799153fb7e
Victor Viale <somebodywas@gmail.com>
2015-11-20 15:14:34 +0100

    Remove confirmation from users

--- db/schema.rb
+++ db/schema.rb
@@ -27,10 +27,6 @@ ActiveRecord::Schema.define(version: 20151119213634) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
     t.integer  "role",                   default: 0,  null: false
   end
 
