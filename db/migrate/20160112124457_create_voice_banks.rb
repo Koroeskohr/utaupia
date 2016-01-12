902586eb7bae9be5c36c28534fcf295bb786d13f
YellowFish <theo.costantini@gmail.com>
2016-01-12 15:55:22 +0100

    Fix bad column name

--- db/migrate/20160112124457_create_voice_banks.rb
+++ db/migrate/20160112124457_create_voice_banks.rb
@@ -1,7 +1,7 @@
 class CreateVoiceBanks < ActiveRecord::Migration
   def change
     create_table :voice_banks do |t|
-    	t.integer :user_id, index: true
+    	t.integer :utauloid_id, index: true
     	t.string :name, null: false
     	t.text :download_link, null: false
       t.timestamps null: false
