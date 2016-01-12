08b1720cfa80af2fa9e9e1a431da7b877976662d
Victor Viale <somebodywas@gmail.com>
2016-01-12 15:17:02 +0100

    Fix enum in utauloid

--- app/models/utauloid.rb
+++ app/models/utauloid.rb
@@ -1,4 +1,6 @@
 class Utauloid < ActiveRecord::Base
+	enum gender: [:male, :female, :undefined, :other]
+  
   validates :name, presence: true, length: { in: 1..60 } 
   validates :japanese_name, length: { in: 1..60 } 
   validates :gender, presence: true, inclusion: { in: genders.keys }
@@ -8,12 +10,11 @@ class Utauloid < ActiveRecord::Base
   validates :creator, length: { in: 1..60 } 
   validates :creator_id, presence: true 
   validates :difficulty, inclusion: { in: 1..5 }
-  validates :wiki_url, format: { in: /\A(http(s)?:\/\/)?utau.wikia.com\/wiki\/.*\z/i},
+  validates :wiki_url, format: { with: /\A(http(s)?:\/\/)?utau.wikia.com\/wiki\/.*\z/i},
                        length: { in: 1..300 }
-  validates :vocadb_url, format: { in: /\A(http(s)?:\/\/)?(www.)?vocadb.net\/.*\z/i},
+  validates :vocadb_url, format: { with: /\A(http(s)?:\/\/)?(www.)?vocadb.net\/.*\z/i},
                          length: { in: 1..300 }
-	
-	enum gender: [:male, :female, :undefined, :other]
+  
 
 	belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
 end
