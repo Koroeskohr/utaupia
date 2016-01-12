7da0c0643415a34fd514070d93563630f60641c3
YellowFish <theo.costantini@gmail.com>
2016-01-12 14:39:59 +0100

    Fix 4f9ecccdd0c40a0afc22d2a5bd2d33237834fc9b Reason: bad type name

--- db/migrate/20160112130924_add_is_append_to_voice_bank.rb
+++ db/migrate/20160112130924_add_is_append_to_voice_bank.rb
@@ -1,5 +1,5 @@
 class AddIsAppendToVoiceBank < ActiveRecord::Migration
   def change
-    add_column :voice_banks, :is_append, :bool, null: false, default: true
+    add_column :voice_banks, :is_append, :boolean, null: false, default: true
   end
 end
