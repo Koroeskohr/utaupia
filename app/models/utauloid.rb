8302650b2431860a148d1a63dabc9319fa55cf68
Victor Viale <somebodywas@gmail.com>
2016-01-12 14:33:39 +0100

    Update utauloid model

--- app/models/utauloid.rb
+++ app/models/utauloid.rb
@@ -1,10 +1,17 @@
 class Utauloid < ActiveRecord::Base
-	validates :name, presence: true
-	validates :gender, presence: true
-	validates :age, numericality: { only_integer: true, greater_than: 1 }
+	validates :name, presence: true, length: { in: 1..255 } 
+  validates :japanese_name, length: { in: 1..255 } 
+	validates :gender, presence: true, inclusion: { in: Utauloid.genders.keys }
+  validates_datetime :vb_release_date
+  validates_datetime :vb_last_update
+  validates :creator, length: { in: 1..60 } 
+  validates :difficulty, inclusion: { in: 1..5 }
+  validates :wiki_url, format: { in: /\A(http(s)?:\/\/)?utau.wikia.com\/wiki\/.*\z/i}
+  validates :vocadb_url, format: { in: /\A(http(s)?:\/\/)?(www.)?vocadb.net\/.*\z/i}
+
 
 	
-	enum gender: [:male, :female]
+	enum gender: [:male, :female, :undefined, :other]
 
 	belongs_to :user
 end
--- db/migrate/20151113140838_create_utauloids.rb
+++ db/migrate/20151113140838_create_utauloids.rb
@@ -2,8 +2,18 @@ class CreateUtauloids < ActiveRecord::Migration
   def change
     create_table :utauloids do |t|
       t.string :name, null: false
+      t.string :japanese_name
       t.integer :gender, null: false # Integer is for enumeration in Utauloid model
-      t.integer :age
+      t.timestamp :vb_release_date
+      t.timestamp :vb_last_update
+      t.boolean :show_appends, null: false
+      t.integer :category_id
+      t.string :creator
+      t.boolean :creator_is_user
+      t.integer :creator_id
+      t.int :difficulty
+      t.text :wiki_url
+      t.text :vocadb_url
 
       t.timestamps null: false
     end
