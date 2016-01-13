e78033de3ee931792bc6f3e3624376becc3a2c05
YellowFish <theo.costantini@gmail.com>
2016-01-13 16:44:50 +0100

    Add UserLinks create, update and destroy routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
   resources :utauloids
   resources :categories
 
+  resources :user_links, only: [:create, :update, :destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
