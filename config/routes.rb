162b52322dcbd201f8d53c4bfad92ceaa9b39e82
Victor Viale <somebodywas@gmail.com>
2016-01-13 13:55:07 +0100

    Refactor routes after merge

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
-  # removes the users namespace to the sign_in and sign_out actions, and renaming them as login and logout
   namespace :admin do
     resources :users
   end
 
-  devise_for :users, :path => '', :path_names => { :sign_in => 'login', :sign_out => 'logout', edit: 'account/edit' }
+  # removes the users namespace to the sign_in and sign_out actions, and renaming them as login and logout
+  devise_for :users, :path => '', :path_names => { :sign_in => 'login', 
+                                                   :sign_out => 'logout', 
+                                                   edit: 'account/edit' }
 
   resources :users, only: [:show, :index, :destroy, :edit]
   root to: 'utauloids#index', as: 'root_path'
