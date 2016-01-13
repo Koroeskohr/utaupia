3f32e3f7c81970aa82d1226648b4ccfc6b193a54
Victor Viale <somebodywas@gmail.com>
2016-01-13 15:30:59 +0100

    Refactor

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
-	validates :name, presence: true, length: { in: 1..100 }
+	validates :name, presence: true, length: { maximum: 100 }
 
   has_many :utauloids
 end
--- app/models/voice_bank.rb
+++ app/models/voice_bank.rb
@@ -1,6 +1,6 @@
 class VoiceBank < ActiveRecord::Base
 	validates :utauloid_id, presence: true
-	validates :name, presence: true, length: {in: 1..255}
+	validates :name, presence: true, length:{ maximum: 255 }
 	validates :download_link, presence: true
 
   belongs_to :utauloid
