f72b368ada0392885a895849285568a73e296add
YellowFish <theo.costantini@gmail.com>
2016-01-15 14:58:28 +0100

    Add methods to UtauloidComments controller

--- app/controllers/utauloid_comments_controller.rb
+++ app/controllers/utauloid_comments_controller.rb
@@ -1,3 +1,13 @@
 class UtauloidCommentsController < ApplicationController
+	before_action :authenticate_user!
 
+
+private
+	def utauloid_comments_params
+		params.require(:utauloid_comment).permit(:message, :utauloid_id)
+	end
+
+	def utauloid_exsits
+		Utauloid.exists?(id: utauloid_comments_params[:utauloid_id])
+	end
 end
