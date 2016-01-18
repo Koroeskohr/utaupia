e7fb18347a8c45c6fbf66772f813e73422bae2df
YellowFish <theo.costantini@gmail.com>
2016-01-18 17:49:20 +0100

    Add avatar dimensions validation

--- app/models/user_info.rb
+++ app/models/user_info.rb
@@ -25,6 +25,18 @@ class UserInfo < ActiveRecord::Base
 
 	validates_attachment_content_type :avatar, content_type: /\Aimage/
 	validates_attachment_size :avatar, { in: 0..500.kilobytes }
+	validate :image_dimensions
 
   accepts_nested_attributes_for :user_links, reject_if: lambda {|attributes| attributes['link'].blank?}, allow_destroy: true
+
+private
+  def image_dimensions
+    required_width  = 150 # Store theses values in a config file
+    required_height = 150
+    dimensions = Paperclip::Geometry.from_file(avatar.queued_for_write[:original].path)
+
+    errors.add(:avatar, "Width must be at max 150px") unless dimensions.width <= required_width
+    errors.add(:avatar, "Height must be at max 150px") unless dimensions.height <= required_height
+  end
+
 end
