837b35cf11040ed6aff74acc86103900b3ae54b9
YellowFish <theo.costantini@gmail.com>
2016-01-18 17:31:14 +0100

    Add paperclip configuration for user_info

--- app/models/user_info.rb
+++ app/models/user_info.rb
@@ -9,5 +9,22 @@ class UserInfo < ActiveRecord::Base
 
 	belongs_to :user
   has_many :user_links
+
+	has_attached_file :avatar,
+		:path => ":rails_root/public/:attachment/:hash_:style.:extension",
+		:url => "/:attachment/:hash_:style.:extension",
+		:hash_secret => RANDOM_SECRET,
+		styles: {
+			medium: "100x",
+			thumb: "100x100#" },
+		:convert_options => {
+			:medium => "-quality 80",
+			:thumb => "-quality 80"
+		},
+		default_url: ":rails_root/public/assets/:attachment/:style/missing.png"
+
+	validates_attachment_content_type :avatar, content_type: /\Aimage/
+	validates_attachment_size :avatar, { in: 0..500.kilobytes }
+
   accepts_nested_attributes_for :user_links, reject_if: lambda {|attributes| attributes['link'].blank?}, allow_destroy: true
 end
