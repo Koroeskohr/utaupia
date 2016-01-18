c354d790119ba779d002a7f80157e2e220e44223
YellowFish <theo.costantini@gmail.com>
2016-01-18 12:38:17 +0100

    Add create and update actions to DifficultyVote

--- app/controllers/difficulty_votes_controller.rb
+++ app/controllers/difficulty_votes_controller.rb
@@ -2,11 +2,28 @@ class DifficultyVotesController < ApplicationController
 	before_action :authenticate_user!
 
 	def create
-		
+		raise ActionController::RoutingError.new('Not Found') unless utauloid_exsits
+
+		@vote = DifficultyVote.new(difficulty_vote_params)
+		@vote.user = current_user
+
+		if @vote.save
+			redirect_to Utauloid.find(difficulty_vote_params[:utauloid_id])
+		else
+			raise ActionController::RoutingError.new('Not Found')
+		end
 	end
 
 	def update
+		raise ActionController::RoutingError.new('Not Found') unless utauloid_exsits && vote_exists
 
+		@vote = DifficultyVote.find(params[:id])
+
+		if @vote.update_attributes(difficulty_vote_params)
+			redirect_to Utauloid.find(difficulty_vote_params[:utauloid_id])
+		else
+			raise ActionController::RoutingError.new('Not Found')
+		end
 	end
 
 private
