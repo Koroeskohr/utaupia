9cfdc1b37e1de50c8178f33ba3fae4d5ad65d27b
YellowFish <theo.costantini@gmail.com>
2016-01-18 12:37:50 +0100

    Add actions and methods to DifficultyVote

--- app/controllers/difficulty_votes_controller.rb
+++ app/controllers/difficulty_votes_controller.rb
@@ -1,3 +1,24 @@
 class DifficultyVotesController < ApplicationController
+	before_action :authenticate_user!
 
+	def create
+		
+	end
+
+	def update
+
+	end
+
+private
+	def difficulty_vote_params
+		params.require(:difficulty_vote).permit(:note, :utauloid_id)
+	end
+
+	def utauloid_exsits
+		Utauloid.exists?(id: difficulty_vote_params[:utauloid_id])
+	end
+
+	def vote_exists
+		DifficultyVote.exists?(id: params[:id], user_id: current_user.id, utauloid_id: difficulty_vote_params[:utauloid_id])
+	end
 end	
