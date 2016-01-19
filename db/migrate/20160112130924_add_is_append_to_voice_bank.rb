e591307c90529f1d53ad7a55998c82b520021d81
Victor Viale <somebodywas@gmail.com>
2016-01-19 13:49:31 +0100

    Correct default value of is append

--- db/migrate/20160112130924_add_is_append_to_voice_bank.rb
+++ db/migrate/20160112130924_add_is_append_to_voice_bank.rb
@@ -1,5 +1,5 @@
 class AddIsAppendToVoiceBank < ActiveRecord::Migration
   def change
-    add_column :voice_banks, :is_append, :boolean, null: false, default: true
+    add_column :voice_banks, :is_append, :boolean, null: false, default: false
   end
 end
