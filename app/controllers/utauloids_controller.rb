a1ebe03b0d2f7d558b46b85f3bc717de0c43818d
YellowFish <theo.costantini@gmail.com>
2016-01-19 11:50:21 +0100

    Add audio_preview to Utauloid

--- app/controllers/utauloids_controller.rb
+++ app/controllers/utauloids_controller.rb
@@ -69,6 +69,7 @@ private
 																		:japanese_name,
 																		:avatar,
 																		:cover,
+																		:audio_preview,
 																		:gender,
 																		:category_id,
 																		{ :voice_language_ids => [] },
