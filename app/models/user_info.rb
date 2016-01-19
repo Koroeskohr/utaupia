09e2cf695b493635f6369c3b7199b9221965d552
YellowFish <theo.costantini@gmail.com>
2016-01-19 10:36:54 +0100

    Change user_info path for avatar

--- app/models/user_info.rb
+++ app/models/user_info.rb
@@ -11,8 +11,8 @@ class UserInfo < ActiveRecord::Base
   has_many :user_links
 
 	has_attached_file :avatar,
-		:path => ":rails_root/public/:attachment/:hash_:style.:extension",
-		:url => "/:attachment/:hash_:style.:extension",
+		:path => ":rails_root/public/users/:attachment/:hash_:style.:extension",
+		:url => "/users/:attachment/:hash_:style.:extension",
 		:hash_secret => RANDOM_SECRET,
 		styles: {
 			medium: "100x",
@@ -25,18 +25,6 @@ class UserInfo < ActiveRecord::Base
 
 	validates_attachment_content_type :avatar, content_type: /\Aimage/
 	validates_attachment_size :avatar, { in: 0..500.kilobytes }
-	validate :image_dimensions
 
   accepts_nested_attributes_for :user_links, reject_if: lambda {|attributes| attributes['link'].blank?}, allow_destroy: true
-
-private
-  def image_dimensions
-    required_width  = 150 # Store theses values in a config file
-    required_height = 150
-    dimensions = Paperclip::Geometry.from_file(avatar.queued_for_write[:original].path)
-
-    errors.add(:avatar, "Width must be at max 150px") unless dimensions.width <= required_width
-    errors.add(:avatar, "Height must be at max 150px") unless dimensions.height <= required_height
-  end
-
 end
