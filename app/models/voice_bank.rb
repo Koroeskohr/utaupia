2ba20923f4c1d8807c80961fd7bfdf223d7d96e3
Victor Viale <somebodywas@gmail.com>
2016-01-21 17:51:03 +0100

    Make sure url has protocol

--- app/models/voice_bank.rb
+++ app/models/voice_bank.rb
@@ -1,7 +1,16 @@
 class VoiceBank < ActiveRecord::Base
+  include UrlHelper
+
+  after_save :add_protocol_to_url
+
 	validates :utauloid_id, presence: true
 	validates :name, presence: true, length: { maximum: 255 }
 	validates :download_link, presence: true, :url => { :allow_blank => true, :no_local => true }
 
   belongs_to :utauloid
+
+  private
+  def add_protocol_to_url
+    self.download_link = url_with_protocol(self.download_link)
+  end
 end
