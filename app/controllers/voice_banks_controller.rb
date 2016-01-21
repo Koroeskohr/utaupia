9b76646301a9d0c47fbe9b8c6ee1fecc7a9df549
Victor Viale <somebodywas@gmail.com>
2016-01-21 15:29:58 +0100

    Correct flow control in vb controller

--- app/controllers/voice_banks_controller.rb
+++ app/controllers/voice_banks_controller.rb
@@ -3,17 +3,18 @@ class VoiceBanksController < ApplicationController
 
   def new
     @utauloid = Utauloid.friendly.find(params[:utauloid_id])
+    return redirect_to :root_path if @utauloid.creator != current_user
     @voice_bank = @utauloid.voice_banks.new
-    redirect_to :root_path if @utauloid.creator != current_user
   end
 
   def edit
     @voice_bank = VoiceBank.find(params[:id])
+    redirect_to @voice_bank.utauloid if @voice_bank.utauloid.creator != current_user
   end
 
   def create
     utauloid = Utauloid.find(params[:voice_bank][:utauloid_id])
-    redirect_to :root_path unless utauloid.creator == current_user
+    return redirect_to :root_path unless utauloid.creator == current_user
     @voice_bank = VoiceBank.new(voice_bank_params)
     @voice_bank.utauloid = utauloid
 
