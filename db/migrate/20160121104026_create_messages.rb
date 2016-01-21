43170782b346468a70fe88740a65308b337de8a9
YellowFish <theo.costantini@gmail.com>
2016-01-21 12:06:17 +0100

    Add message table fields

--- db/migrate/20160121104026_create_messages.rb
+++ db/migrate/20160121104026_create_messages.rb
@@ -1,7 +1,13 @@
 class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
-
+    	t.integer :user_id, null: false
+    	t.integer :message_type, null: false
+    	t.text :message, null: false
+    	t.integer :author_id
+    	t.boolean :deleted
+    	t.boolean :seen
+      t.timestamps null: false
     end
   end
 end
