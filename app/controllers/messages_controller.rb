02d6b55a2df4a7be21ae988a14490b09dbb84f61
YellowFish <theo.costantini@gmail.com>
2016-01-21 15:20:47 +0100

    Change messages controller multiple deleting

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,10 +2,6 @@ class MessagesController < ApplicationController
 	before_action :authenticate_user!
 
 	def index
-		if !params[:delete_messages].blank?
-			Message.where(id: get_messages_ids_params[:delete_messages]).update_all({ deleted: true })
-		end
-
 		if !params[:filters].blank?
 			@messages = current_user.messages.where(deleted: false, message_type: get_filtered_message_types(params[:filters]))
 		else
@@ -18,11 +14,18 @@ class MessagesController < ApplicationController
 	end
 
 	def create
+		
+	end
 
+	def add_multiple_to_bin
+		if !params[:delete_messages].blank?
+			current_user.messages.where(id: get_messages_ids_params[:delete_messages]).update_all({ deleted: true })
+		end
+		redirect_to messages_path
 	end
 
 	def add_to_bin
-		Message.find(params[:id]).update_attributes({ deleted: true })
+		current_user.messages.find(params[:id]).update_attributes({ deleted: true })
 		redirect_to messages_path
 	end
 
