62653d38d6a79cdba9ca6071695c88e39b29fc59
YellowFish <theo.costantini@gmail.com>
2016-01-21 14:38:27 +0100

    Add some actions to messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,34 @@
 class MessagesController < ApplicationController
+	before_action :authenticate_user!
+
+	def index
+		if !params[:filters].blank?
+			@messages = current_user.messages.where(deleted: false, message_type: get_filtered_message_types(params[:filters]))
+		else
+			@messages = current_user.messages.where(deleted: false)
+		end
+	end
+
+	def show
+
+	end
+
+	def create
+
+	end
+
+	def add_to_bin
+		Message.find(params[:id]).update_attributes({ deleted: true })
+		redirect_to messages_path
+	end
+
+private
+	def get_filtered_message_types(filters)
+		r = []
+		r.push(Message.message_types[:notif_new_comment]) if filters.include?("new_comment")
+		r.push(Message.message_types[:notif_new_favorite]) if filters.include?("new_favorite")
+		r.push(Message.message_types[:notif_new_update]) if filters.include?("favorite_updated")
+
+		r
+	end
 end
