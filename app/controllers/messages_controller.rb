713bcfb2af76b1705707b69cf627fdd9c7205c6e
YellowFish <theo.costantini@gmail.com>
2016-01-21 15:04:12 +0100

    Add multiple deleting in messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,10 @@ class MessagesController < ApplicationController
 	before_action :authenticate_user!
 
 	def index
+		if !params[:delete_messages].blank?
+			Message.where(id: get_messages_ids_params[:delete_messages]).update_all({ deleted: true })
+		end
+
 		if !params[:filters].blank?
 			@messages = current_user.messages.where(deleted: false, message_type: get_filtered_message_types(params[:filters]))
 		else
@@ -23,6 +27,10 @@ class MessagesController < ApplicationController
 	end
 
 private
+	def get_messages_ids_params
+		params.permit({ :delete_messages => [] },)
+	end
+
 	def get_filtered_message_types(filters)
 		r = []
 		r.push(Message.message_types[:notif_new_comment]) if filters.include?("new_comment")
