fd01b74fa4792566aded584423c73c4fe265691c
YellowFish <theo.costantini@gmail.com>
2016-01-22 14:07:38 +0100

    Add ajax for difficulty votes

--- app/controllers/difficulty_votes_controller.rb
+++ app/controllers/difficulty_votes_controller.rb
@@ -1,15 +1,21 @@
 class DifficultyVotesController < ApplicationController
+	before_action :ensure_xhr
 	before_action :authenticate_user!
 	before_action :utauloid_exists
 	before_action :vote_exists, only: [:update]
 
 	def create
+		if current_user.difficulty_votes.exists?(utauloid_id: difficulty_vote_params[:utauloid_id])
+			if !current_user.difficulty_votes.where(utauloid_id: difficulty_vote_params[:utauloid_id]).destroy
+				render :json => { status: 404 }
+			end
+		end
 		@vote = current_user.difficulty_votes.new(difficulty_vote_params)
 
 		if @vote.save
-			redirect_to Utauloid.find(difficulty_vote_params[:utauloid_id])
+			render :json => { status: :ok, message: "Success" }
 		else
-			raise ActionController::RoutingError.new('Not Found')
+			render :json => { status: 404 }
 		end
 	end
 
@@ -17,9 +23,9 @@ class DifficultyVotesController < ApplicationController
 		@vote = current_user.difficulty_votes.find(params[:id])
 
 		if @vote.update_attributes(difficulty_vote_params)
-			redirect_to Utauloid.find(difficulty_vote_params[:utauloid_id])
+			render :json => { status: :ok, message: "Success" }
 		else
-			raise ActionController::RoutingError.new('Not Found')
+			render :json => { status: 404 }
 		end
 	end
 
@@ -30,13 +36,13 @@ private
 
 	def utauloid_exists
 		if !Utauloid.exists?(id: difficulty_vote_params[:utauloid_id])
-			raise ActionController::RoutingError.new('Not Found')
+			render :json => { status: 404 }
 		end
 	end
 
 	def vote_exists
 		if !current_user.difficulty_votes.exists?(id: params[:id])
-			raise ActionController::RoutingError.new('Not Found')
+			render :json => { status: 404 }
 		end
 	end
 end	
