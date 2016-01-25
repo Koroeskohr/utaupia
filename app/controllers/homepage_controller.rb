7186e84de9e8a58ecd05398454f5c440c109a853
YellowFish <theo.costantini@gmail.com>
2016-01-25 10:54:19 +0100

    Add content to homepage index

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,3 +1,8 @@
 class HomepageController < ApplicationController
+	def index
+		homepage_params = Homepage.first
 
+		@utauloid_showcase = Utauloid.find(homepage_params[:utauloid_showcase])
+		@utauloids = Utauloid.limit(5)
+	end
 end
