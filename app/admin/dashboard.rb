9a713308840a4f1e7e4ed988624382d5f25803cb
Victor Viale <somebodywas@gmail.com>
2016-01-26 21:42:33 +0100

    Create simple dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,31 +3,49 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
-    end
-
     # Here is an example of a simple dashboard with columns and panels.
     #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    columns do
+      column do
+        panel "Recent reports" do
+          ul do
+            Report.last(5).map do |report|
+              li link_to report.reportable.name, polymorphic_path([:admin, report.reportable])
+            end
+          end
+        end
+      end
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
+      column do
+        panel "Stats" do
+          ul do
+            li "#{User.count} users"
+            li "#{VoiceBank.count} voice banks over #{Utauloid.count} utauloids"
+          end
+        end
+      end
+    end # top row
+
+    columns do
+      column do
+        panel "Last 10 utauloids added" do
+          ul do
+            Utauloid.last(10).map do |utauloid|
+              li link_to utauloid.name, admin_utauloid_path(utauloid)
+            end
+          end
+        end
+      end
+
+      column do
+        panel "Last 10 users registered" do
+          ul do
+            User.last(10).map do |user|
+              li link_to user.nickname, admin_user_path(user)
+            end
+          end
+        end
+      end
+    end # bottom row
   end # content
 end
