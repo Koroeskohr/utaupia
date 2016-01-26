60e2d13737893f1135caa16fc0f58e27ae9d4200
Victor Viale <somebodywas@gmail.com>
2016-01-26 17:47:35 +0100

    Report belongs to user

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,7 +1,8 @@
 class Report < ActiveRecord::Base
-	belongs_to :reportable, polymorphic: true
+  belongs_to :reportable, polymorphic: true
+  belongs_to :user
 
-	validates :reportable_id, presence: true
-	validates :reportable_type, presence: true
-	validates :user_id, presence: true, uniqueness: { scope: [:reportable_id, :reportable_type] }
+  validates :reportable_id, presence: true
+  validates :reportable_type, presence: true
+  validates :user_id, presence: true, uniqueness: { scope: [:reportable_id, :reportable_type] }
 end
