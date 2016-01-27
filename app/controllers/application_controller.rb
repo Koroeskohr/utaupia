7148e67fc50ccc10489dff0bc1f38c076fac5699
Victor Viale <somebodywas@gmail.com>
2016-01-27 17:40:55 +0100

    Hack : save from big errors by returning home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  rescue_from NoMethodError, with: :not_found
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:nickname, :email, :password, :password_confirmation) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:nickname, :password, :password_confirmation, :current_password) }
@@ -24,6 +26,12 @@ class ApplicationController < ActionController::Base
       flash[:error] = "This account has been suspended."
       redirect_to :root_path
     end
+  end
+
+  def not_found
+    message = "An unexpected error happened"
+    logger.error message
+    redirect_to :root_path, :flash => { :error => message }
   end
 
 end
