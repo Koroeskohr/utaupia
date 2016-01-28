110b14ee5faa072d70cfffa0442ef1d2108a1f3f
Victor Viale <somebodywas@gmail.com>
2016-01-28 16:50:06 +0100

    remove debug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  rescue_from NoMethodError, with: :not_found
+  # rescue_from NoMethodError, with: :not_found
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:nickname, :email, :password, :password_confirmation) }
