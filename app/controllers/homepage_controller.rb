ed4abfbaf999c786f2882ecfb97be521e02ffadb
Victor Viale <somebodywas@gmail.com>
2016-01-28 15:46:28 +0100

    fix homepage creation on seed

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -2,8 +2,8 @@ class HomepageController < ApplicationController
 	def index
 		homepage_params = Homepage.first
 
-		if !homepage_params.utauloid_showcase.blank?
-			@utauloid_showcase = Utauloid.find(homepage_params.utauloid_showcase)
+		if !homepage_params.utauloid_showcase.nil?
+			@utauloid_showcase = homepage_params.utauloid_showcase
 		end
 		@utauloids = Utauloid.order("id DESC").limit(5)
 	end
